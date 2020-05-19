fac4551380da0d46d61ca817028648e3eb8536de
MkItSimple <choanania1@gmail.com>
2020-05-19 16:26:25 +0800

    16-pro

--- app/src/main/java/com/example/androidnotification/ProfileActivity.java
+++ app/src/main/java/com/example/androidnotification/ProfileActivity.java
@@ -2,30 +2,47 @@ package com.example.androidnotification;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String NODE_USERS = "users";
     private FirebaseAuth mAuth;
+    private List<User> userList;
+
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        progressBar = findViewById(R.id.progressbar);
         mAuth = FirebaseAuth.getInstance();
+
+        loadUsers();
 
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -34,11 +51,44 @@ public class ProfileActivity extends AppCompatActivity {
                         if (task.isSuccessful()){
                             String token = task.getResult().getToken();
                             saveToken(token);
-                        }else{
-
                         }
                     }
                 });
+    }
+
+    private void loadUsers() {
+        progressBar.setVisibility(View.VISIBLE);
+        userList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference("users");
+        dbUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                progressBar.setVisibility(View.GONE);
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot dsUser: dataSnapshot.getChildren()){
+
+                        User user = dsUser.getValue(User.class);
+                        userList.add(user);
+
+                    }
+
+                    UserAdapter adapter = new UserAdapter(ProfileActivity.this, userList);
+                    recyclerView.setAdapter(adapter);
+
+                }else{
+                    Toast.makeText(ProfileActivity.this, "No user found", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
