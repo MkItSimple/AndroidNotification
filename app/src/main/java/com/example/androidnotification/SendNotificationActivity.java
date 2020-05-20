221e17afbb7ab205f51cecff5a1734518b20c17b
MkItSimple <choanania1@gmail.com>
2020-05-21 06:32:16 +0800

    06-PushNotification

--- app/src/main/java/com/example/androidnotification/SendNotificationActivity.java
+++ app/src/main/java/com/example/androidnotification/SendNotificationActivity.java
@@ -9,6 +9,6 @@ public class SendNotificationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_notification);
+        setContentView(R.layout.activity_send_notification); //
     }
 }
